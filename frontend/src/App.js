9ff42d8a00b5539a491c322288d4123811108b26
012SwastikKumar <swastiktovisit1234@gmail.com>
2023-02-23 10:02:56 +0530

    discover-skeleton

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Landing from './Landing';
+// import Login from './Login';
+import Discover from './Discover';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Landing/>
+      {/* <Login/> */}
+      <Discover/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Filters/SemesterFilter.js
@@ -0,0 +1,26 @@
+import React, { useState }from "react";
+import "./SemesterFilter.css";
+
+const SemesterFilter = (props) => {
+  const sectorRadioList = ["1", "2", "3", "4", "5"];
+  // const [sem, setSem] = useState();
+  const onChangeHandler = (event) => {
+  //   setSem(event.target.value)
+    event.preventDefault()
+    // console.log(event.target.value)
+    props.filterVaueSelected(event.target.value)
+  }
+
+  return (
+    <div className="semesterfilter">
+      <h3>Semester</h3>
+      <select className="semesterList" onChange={onChangeHandler}>
+        {sectorRadioList.map((item, index) => {
+          return <option value={item}>{item}</option>;
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default SemesterFilter;
